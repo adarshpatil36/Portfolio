45b9d7b231a1e5df861ae3849802fe7100707770
adarsh.patil <adarsh.patil@globant.com>
2021-03-11 08:47:47 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+var i = 0;
+var txt = ['practicing minimalist.','lifelong learner.', 'building something Super.', 'sharing my thoughts.'];
+var nextStatement=0;
+var typingSpeed = 100;
+var clearingSpeed = 50;
+
+var cursor = true;
+var speed = 500;
+
+setInterval(() => {
+   if(cursor) {
+     document.getElementById('cursor').style.opacity = 0;
+     cursor = false;
+   }else {
+     document.getElementById('cursor').style.opacity = 1;
+     cursor = true;
+   }
+}, speed);
+
+
+function typeWriter() {
+  if (i <= txt[nextStatement].length) {
+    document.getElementById("demo").innerHTML = txt[nextStatement].split('').slice(0,i).join('');
+    i++;
+    setTimeout(typeWriter, typingSpeed);
+  } else{
+    if(nextStatement<3)
+      setTimeout(clearWriter, 2000);
+	}
+}
+function clearWriter(){
+	if(i>0){
+      document.getElementById("demo").innerHTML = txt[nextStatement].split('').slice(0,i).join('');
+	i--;
+	setTimeout(clearWriter, clearingSpeed);
+	} else{
+    	nextStatement++
+        var id = setTimeout(typeWriter, typingSpeed);
+        if(nextStatement===4)
+                clearTimeout(id);       
+    }
+}
+
+
 function App() {
+  useEffect(()=>{
+    {setTimeout(typeWriter, typingSpeed)}
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p className="main-content">
+        <samp>Ad@rsh Patil,&nbsp;</samp>
+        <samp>Front-end developer,&nbsp;</samp>
+        <samp id="demo"> </samp>
+        <samp id="cursor">|</samp>
+      </p>
+      <table>
+        <tr>
+          <td>
+            Hello
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Hello
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Hello
+          </td>
+        </tr>
+        
+      </table>
+    </>
   );
 }
 
